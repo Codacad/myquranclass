f9ee7ad00d2d6706e5ffb55d42b7f1fd9ef4bc19
Mohd Rizwan <iamrizwan40@gmail.com>
2023-08-26 11:57:40 +0530

    updages

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import LandingPage from './components/LandingPage'
 import AccessLeanring from './components/AccessLeanring'
 import ChooseToLearn from './components/ChooseToLearn'
 import PrivateClass from './components/PrivateClass'
+import TutoringService from './components/TutoringService'
 const App = () => {
   return (
     <div>
       <Navbar />
       <LandingPage />
+      <TutoringService />
       <AccessLeanring />
       <ChooseToLearn />
       <PrivateClass />
+      
     </div>
   )
 }
---
+++ src/components/TutoringService.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../css/TutoringService.css'
+
+const TutoringService = () => {
+  return (
+    <>
+      <div className="tutoring-service">
+        <div className="headings">
+          <h3>Happy Service</h3>
+          <h1>How is our tutoring service?</h1>
+        </div>
+        <div className="services">
+          <div className="service">
+              <h1>🧑🏽‍🏫</h1>
+              <h2>Effective teaching methods</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, suscipit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, laudantium!</p>
+              <a href="#" className="read-more">Read More</a>
+          </div>
+          <div className="service">
+              <h1>🎆</h1>
+              <h2>Quality of education</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, commodi.</p>
+              <a href="#" className="read-more">Read More</a>
+          </div>
+          <div className="service">
+              <h1>🧩</h1>
+              <h2>Tutor-student interaction</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, similique?</p>
+              <a href="#" className="read-more">Read More</a>
+          </div>
+          <div className="service">
+              <h1>👍🏽</h1>
+              <h2>Feedback and improvement</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, optio.</p>
+              <a href="#" className="read-more">Read More</a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TutoringService

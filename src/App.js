e0e9953ad473b6a6b405766c7bd3719ddcf9e1ba
Mohd Rizwan <iamrizwan40@gmail.com>
2023-09-10 14:45:05 +0530

    replace BrowserRouter with Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './css/App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Message from './components/Message'
 import Main from './components/Main'
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path='/' exact element={<Main />}/>
           <Route path='/message' element={<Message />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
